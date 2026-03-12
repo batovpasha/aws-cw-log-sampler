92e40ccce4c43509fce4c709c0850df0d414c492
Pavlo Batov <batovpasha@gmail.com>
2026-03-12 23:07:27 +0200

    feat: add basic service command implementation

---
+++ cmd/service/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	"github.com/batovpasha/aws-cw-log-sampler/internal/sample"
+	"github.com/robfig/cron/v3"
+	"golang.org/x/sync/errgroup"
+)
+
+// GetLogEvents has the lowest TPS - 10, and it's a bottleneck.
+// See limits of other APIs here: https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/cloudwatch_limits_cwl.html
+const concurrencyLimit = 10
+
+type Config struct {
+	SrcGroups []string `json:"srcGroups"`
+	DstGroup  string   `json:"dstGroup"`
+	Cron      string   `json:"cron"`
+}
+
+func main() {
+	data, err := os.ReadFile("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var appCfg Config
+	if err := json.Unmarshal(data, &appCfg); err != nil {
+		log.Fatal(err)
+	}
+
+	parser := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
+	sched, err := parser.Parse(appCfg.Cron)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	now := time.Now()
+	next1 := sched.Next(now)
+	next2 := sched.Next(next1)
+	interval := next2.Sub(next1)
+	cutoff := now.Add(-interval).UnixMilli()
+
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := cloudwatchlogs.NewFromConfig(cfg)
+
+	var g errgroup.Group
+	g.SetLimit(concurrencyLimit)
+
+	for _, srcGroup := range appCfg.SrcGroups {
+		g.Go(func() error {
+			if err := sample.ProcessLogGroup(ctx, client, cutoff, srcGroup, appCfg.DstGroup); err != nil {
+				fmt.Printf("error processing log group %s: %v\n", srcGroup, err)
+			}
+			return nil
+		})
+	}
+
+	_ = g.Wait()
+}
