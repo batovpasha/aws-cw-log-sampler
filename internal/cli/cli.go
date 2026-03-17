bf10f4e547e0e2122034335534814f60d2693c20
Pavlo Batov <batovpasha@gmail.com>
2026-03-17 22:39:16 +0200

    feat: add package for working with cli flags

---
+++ internal/cli/cli.go
@@ -0,0 +1,49 @@
+package cli
+
+import (
+	"errors"
+	"flag"
+)
+
+type CommonFlags struct {
+	LogGroupNamePattern  string
+	DstGroup             string
+	Type                 string
+	RandLogStreamsNumber int
+}
+
+func RegisterCommonFlags(fs *flag.FlagSet) *CommonFlags {
+	f := &CommonFlags{}
+	fs.StringVar(&f.LogGroupNamePattern, "logGroupNamePattern", "", "log group name pattern")
+	fs.StringVar(&f.DstGroup, "dstGroup", "", "destination log group")
+	fs.StringVar(&f.Type, "type", "", "sampling type")
+	fs.IntVar(&f.RandLogStreamsNumber, "randLogStreamsNumber", 0, "number of random log streams")
+	return f
+}
+
+func ValidateCommonFlags(flags *CommonFlags) error {
+	var errs []error
+
+	if flags.LogGroupNamePattern == "" {
+		errs = append(errs, errors.New("--logGroupNamePattern is required"))
+	}
+	if flags.DstGroup == "" {
+		errs = append(errs, errors.New("--dstGroup is required"))
+	}
+
+	switch flags.Type {
+	case "rand-log-streams":
+		if flags.RandLogStreamsNumber < 1 || flags.RandLogStreamsNumber > 100 {
+			errs = append(
+				errs,
+				errors.New("--randLogStreamsNumber should be an integer less than 100"),
+			)
+		}
+	case "":
+		errs = append(errs, errors.New("--type is required"))
+	default:
+		errs = append(errs, errors.New("--type has invalid value"))
+	}
+
+	return errors.Join(errs...)
+}
